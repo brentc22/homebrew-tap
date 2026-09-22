d57c075a308ecb979c8855bf0aee504a3e8d1a69
Brent Ceulemans <brent.ceulemans@icloud.com>
2026-09-22 16:37:35 +0200

    chore: catchbox 1.0.2

--- Formula/catchbox.rb
+++ Formula/catchbox.rb
@@ -1,8 +1,8 @@
 class Catchbox < Formula
   desc "Disposable inboxes for developers, with the code and the action link pulled out"
   homepage "https://github.com/brentc22/catchbox"
-  url "https://github.com/brentc22/catchbox/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "34278997e20641dda30741cdf400df82cae1c6071395c3eb4c8b7076ee274cbd"
+  url "https://github.com/brentc22/catchbox/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "b07825460b56cc1ec7c7c75715f43bef1d27009c583753c38264bde06acb1a9b"
   license "MIT"
   head "https://github.com/brentc22/catchbox.git", branch: "main"
 
